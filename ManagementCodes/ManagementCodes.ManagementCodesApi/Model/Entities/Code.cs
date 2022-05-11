cd3d97e16375a85056ffcf4311d0d420aa74bc82
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-05-11 21:13:06 +0300

    added class Code

---
+++ ManagementCodes/ManagementCodes.ManagementCodesApi/Model/Entities/Code.cs
@@ -0,0 +1,8 @@
+﻿namespace ManagementCodes.ManagementCodesApi.Model.Entities;
+
+public class Code
+{
+    public int Id { get; set; }
+    public string Value { get; set; }
+    public string Name { get; set; }
+}
