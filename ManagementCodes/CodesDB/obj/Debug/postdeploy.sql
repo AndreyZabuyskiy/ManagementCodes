d12bfbb94c9d94b3cf9edf9d531c0a21ec848b89
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-05-11 20:43:16 +0300

    added file publish

---
+++ ManagementCodes/CodesDB/obj/Debug/postdeploy.sql
@@ -0,0 +1,7 @@
+﻿IF NOT EXISTS (select 1 from dbo.[Code])
+BEGIN
+	INSERT INTO dbo.[Code] ([Value], [Name])
+	VALUES ('328', 'Код'), ('050', 'Водафон'),
+		('852', 'SomeValue'), ('452', 'Coffee')
+END
+GO
