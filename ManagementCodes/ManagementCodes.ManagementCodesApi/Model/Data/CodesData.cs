9e29f166e47dc791c7a6a3dc5bfea691c0f3573b
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-05-12 08:37:25 +0300

    added class CodesData

---
+++ ManagementCodes/ManagementCodes.ManagementCodesApi/Model/Data/CodesData.cs
@@ -0,0 +1,17 @@
+﻿using ManagementCodes.ManagementCodesApi.Model.DataAccess;
+using ManagementCodes.ManagementCodesApi.Model.Entities;
+
+namespace ManagementCodes.ManagementCodesApi.Model.Data;
+
+public class CodesData
+{
+    public readonly ISqlDataAccess _db;
+
+    public CodesData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public async Task<IEnumerable<Code>> GetCodesAsync() =>
+        await _db.LoadData<Code, dynamic>("spCode_GetAll", new { });
+}
